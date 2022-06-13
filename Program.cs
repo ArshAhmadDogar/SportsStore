1ef3bd788821e8ec3a46233405735e7e779683fc
ArshAhmadDogar <aroshash196@gmail.com>
2022-06-13 18:34:51 +0500

    Application Ready to Deploy

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SportsStore.Models;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<StoreDbContext>(opts => {
@@ -7,23 +8,52 @@ builder.Services.AddDbContext<StoreDbContext>(opts => {
  builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
 });
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+builder.Services.AddScoped<IOrderRepository, EFOrderRepository>();
 builder.Services.AddRazorPages();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
+builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+ options.UseSqlServer(
+ builder.Configuration["ConnectionStrings:IdentityConnection"]));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();
+
+
 var app = builder.Build();
+
+if (app.Environment.IsProduction()) {
+ app.UseExceptionHandler("/error");
+}
+app.UseRequestLocalization(opts => {
+ opts.AddSupportedCultures("en-US")
+ .AddSupportedUICultures("en-US")
+ .SetDefaultCulture("en-US");
+});
+
 app.UseStaticFiles();
 app.UseSession();
 
-app.MapControllerRoute("catpage", "{category}/Page{productPage:int}",new { Controller = "Home", action = "Index" });
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapControllerRoute("page", "Page{productPage:int}",new { Controller = "Home", action = "Index", productPage = 1 });
-
-app.MapControllerRoute("category", "{category}",new { Controller = "Home", action = "Index", productPage = 1 });
-
-app.MapControllerRoute("pagination","Products/Page{productPage}",new { Controller = "Home", action = "Index", productPage = 1 });
-
+app.MapControllerRoute("catpage",
+ "{category}/Page{productPage:int}",
+ new { Controller = "Home", action = "Index" });
+app.MapControllerRoute("page", "Page{productPage:int}",
+ new { Controller = "Home", action = "Index", productPage = 1 });
+app.MapControllerRoute("category", "{category}",
+ new { Controller = "Home", action = "Index", productPage = 1 });
+app.MapControllerRoute("pagination",
+ "Products/Page{productPage}",
+ new { Controller = "Home", action = "Index", productPage = 1 });
 app.MapDefaultControllerRoute();
 app.MapRazorPages();
-SeedData.EnsurePopulated(app);
+app.MapBlazorHub();
+app.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
 
+SeedData.EnsurePopulated(app);
+IdentitySeedData.EnsurePopulated(app);
 app.Run();
